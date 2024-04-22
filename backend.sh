e1ab8ead530643407910739829bebd7c6d2fa4fc
chandureddy08 <chandrareddyerigela@gmail.com>
2024-04-22 13:32:06 -0400

    Expense project

--- backend.sh
+++ backend.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ USERID -ne 0 ]
+then
+  echo "please run this script with root access"
+  exit 5
+else
+  echo "You are root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2...$R FAILURE $N"
+      exit 2
+    else
+      echo -e "$2...$G SUCCESS $N"
+    fi 
+}
+
+dnf module disable nodejs -y &>>LOGFILE
+VALIDATE $? ""Disabling Node js"
---
+++ mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%s)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter DB Password:"
+read -s mysql_root_password
+
+if [ USERID -ne 0]
+   then
+   echo "Run this script with root access"
+else
+    echo "You are root user"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo -e "$2...$R FAILURE $N"
+      exit 2
+    else
+      echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+dnf install mysql-server -y &>>LOGFILE
+VALIDATE $? "Installing Mysql server"
+
+systemctl enable mysqld &>>LOGFILE
+VALIDATE $? "Enabling Mysql"
+
+systemctl start mysqld &>>LOGFILE
+VALIDATE $? "Starting Mysql"
+
+mysql -h db.chandureddy.online -uroot -p${mysql_root_password} -e 'show databases;' &>>LOGFILE
+if [ $? -ne 0 ]
+then
+  mysql_secure_installation --set-root-pass ${mysql_root_password} &>>LOGFILE
+  VALIDATE $? "MySQL root password setup"
+else
+  echo -e "MySQL root passwordis already set up...$Y SKIPPING $N"
+fi

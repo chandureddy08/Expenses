0b3b8521e2f5e91aad569e0f015e8c1be81304ca
chandureddy08 <chandrareddyerigela@gmail.com>
2024-04-24 13:18:28 -0400

    Expense project

---
+++ abc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COURSE="DevOps from current script"
+echo "Before calling other script ,Course: $COURSE"
+echo "Process id of current shell script: $$"
+
+./xyz.sh
+
+echo "After calling other script ,Course: $COURSE"
